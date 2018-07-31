127e62daf003fa0c938809efce030028ad10bb67
EunHyeJung <ehye826@gmail.com>
2018-07-31 15:18:34 +0900

    Modified templates to display notes from database

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 
 # Create your views here.
 
 # 요청(request)를 넘겨받아 render 메서드를 호출함.
 # 이 함수를 호출하여 받은(return) blog/post_list.html 템플릿을 보여줌.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', { 'posts' : posts})
